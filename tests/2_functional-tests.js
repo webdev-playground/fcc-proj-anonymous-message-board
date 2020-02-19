78332134f46530d9ef91cd44973dfe5ae197ebf5
Glitch (fcc-proj-anonymous-message-board) <none>
2020-02-19 23:52:16 +0000

    📰🐟 Checkpoint ./tests/2_functional-tests.js:11249790/3601

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,67 +1,55 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
+var chaiHttp = require("chai-http");
+var chai = require("chai");
 var assert = chai.assert;
-var server = require('../server');
+var server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite("Functional Tests", function() {
   this.timeout(5000);
-  
+
   // Drop databases
   //IP.collection.drop();
   //Stock.collection.drop();
 
-  suite('API ROUTING FOR /api/threads/:board', function() {
-    
-    suite('POST', function() {
-      chai.request(server).post('/api/threads/test').send({ text: 'This is a post', delete_password: 'password' }).end((err, res) => {
-        const board = r
+  suite("API ROUTING FOR /api/threads/:board", function() {
+    suite("POST", function() {
+      test("redirect to /b/test after posting thread", function(done) {
+        chai
+          .request(server)
+          .post("/api/threads/test")
+          .send({ text: "This is a thread", delete_password: "password" })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            expect(res).to.redirectTo("/b/test");
+
+            done();
+          });
       });
     });
-    
-    suite('GET', function() {
-      
-    });
-    
-    suite('DELETE', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
-    });
-    
 
-  });
-  
-  suite('API ROUTING FOR /api/replies/:board', function() {
-    
-    suite('POST', function() {
-      
-    });
-    
-    suite('GET', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
-    });
-    
-    suite('DELETE', function() {
-      
-    });
-    
+    suite("GET", function() {});
+
+    suite("DELETE", function() {});
+
+    suite("PUT", function() {});
   });
 
+  suite("API ROUTING FOR /api/replies/:board", function() {
+    suite("POST", function() {});
+
+    suite("GET", function() {});
+
+    suite("PUT", function() {});
+
+    suite("DELETE", function() {});
+  });
 });
