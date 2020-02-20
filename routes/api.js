c03994c6d40ff176ae510c5c8f05814896b7ee94
Glitch (fcc-proj-anonymous-message-board) <none>
2020-02-20 23:56:52 +0000

    👕🍶 Checkpoint ./routes/api.js:11249790/2507

--- routes/api.js
+++ routes/api.js
@@ -8,23 +8,39 @@
 
 "use strict";
 
-const mongoose = require('mongoose');
-const Board = require('../models/board');
-const Thread = require('../models/thread');
-const Reply = require('../models/reply');
+const mongoose = require("mongoose");
+const Board = require("../models/board");
+const Thread = require("../models/thread");
+const Reply = require("../models/reply");
 
 module.exports = function(app) {
-  app.route("/api/threads/:board").post(async (req, res) => {
-    const { board } = req.params;
+  app
+    .route("/api/threads/:board")
+    .post(async (req, res) => {
+      const { board } = req.params;
+
+      const { text, delete_password } = req.body;
+      const newThread = new Thread({ text, delete_password });
+
+      try {
+        const updatedBoard = await Board.findOneAndUpdate(
+          { name: board },
+          { $push: { threads: newThread } },
+          { new: true, upsert: true }
+        );
+      } catch (err) {
+        return res.status(400).json({ error: err.message });
+      }
+
+      res.redirect(`/b/${board}`);
+    })
+    .get(async (req, res) => {
+      const { board } = req.params;
     
-    const { text, delete_password } = req.body;
-    const newThread = new Thread({ text, delete_password });
+      try {
+        
+      }
     
-    try {
-      const board = await Board.findOneAndUpdate()
-    }
-    
-    res.redirect(`/b/${board}`);
   });
 
   app.route("/api/replies/:board");
