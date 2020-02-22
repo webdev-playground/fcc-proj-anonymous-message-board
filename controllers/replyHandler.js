2f1b6de26c8a281cc1070201382462aefa93920a
Glitch (fcc-proj-anonymous-message-board) <none>
2020-02-22 08:48:57 +0000

    🍨💼 Checkpoint ./controllers/replyHandler.js:11249790/389

--- controllers/replyHandler.js
+++ controllers/replyHandler.js
@@ -5,5 +5,21 @@ exports.createReply = (req, res) => {
   const Thread = ThreadModel.setBoard(board);
   
   text, delete_password, thread_id
+  const { text, delete_password, thread_id } = req.body;
+  
+  try {
+    Thread.findOneAndUpdate(
+      { thread_id },
+      {
+        $push: {
+          replies: { text, delete_password }
+        }
+      },
+      { new: true }
+    )  
+  } catch (err) {
+    
+  }
+  
   bumped_on <- thread
 }
