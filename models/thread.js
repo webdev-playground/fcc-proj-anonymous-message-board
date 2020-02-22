dc060e7272e7e478787dee82ece7f6dea5418948
Glitch (fcc-proj-anonymous-message-board) <none>
2020-02-22 05:09:00 +0000

    👫🚡 Checkpoint ./routes/api.js:11249790/997 ./models/thread.js:11249790/502 ./controllers/threadHandler.js:11249790/3882

--- models/thread.js
+++ models/thread.js
@@ -1,6 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const replySchema = Schema({
+  text: {
+    type: String,
+    required: true
+  },
+  delete_password: {
+    type: String,
+    required: true
+  },
+  created_on: {
+    type: Date,
+    default: Date.now()
+  },
+  reported: {
+    type: Boolean,
+    default: false
+  }
+});
+
 const threadSchema = Schema({
   text: {
     type: String,
@@ -22,10 +41,11 @@ const threadSchema = Schema({
     type: Boolean,
     default: false
   },
-  replies: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Reply'
-  }]
+  replies: [replySchema]
 });
 
-module.exports = mongoose.model('Thread', threadSchema);
+exports = module.exports = mongoose.model('Thread', threadSchema);
+
+exports.setBoard = function(boardName) {
+  return mongoose.model('Thread', threadSchema, boardName);
+}
--- routes/api.js
+++ routes/api.js
@@ -9,43 +9,13 @@
 "use strict";
 
 const mongoose = require("mongoose");
-const Board = require("../models/board");
-const Thread = require("../models/thread");
-const Reply = require("../models/reply");
+const threadHandler = require('../controllers/threadHandler');
 
 module.exports = function(app) {
   app
     .route("/api/threads/:board")
-    .post(async (req, res) => {
-      const { board } = req.params;
-
-      const { text, delete_password } = req.body;
-      const newThread = new Thread({ text, delete_password });
-
-      try {
-        const updatedBoard = await Board.findOneAndUpdate(
-          { name: board },
-          { $push: { threads: newThread } },
-          { new: true, upsert: true }
-        );
-      } catch (err) {
-        return res.status(400).json({ error: err.message });
-      }
-
-      res.redirect(`/b/${board}`);
-    })
-    .get(async (req, res) => {
-      const { board } = req.params;
-    
-      try {
-        const     
-      } catch (err) {
-        return res.status(400).json({ error: err.message });
-      }
-    
-      return 
-    
-  });
+    .post(threadHandler.createThread)
+    .get(threadHandler.listThreads);
 
   app.route("/api/replies/:board");
 };
